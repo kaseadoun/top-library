c4d3216d36e90902f182dd4b472f3dcf11a1cc34
Karsten <karstenkleung@gmail.com>
2023-09-13 15:03:24 -0400

    Set Book constructor and create delete button for each entry

--- script/script.js
+++ script/script.js
@@ -1,15 +1,38 @@
+const addBookButton = document.getElementById("addBook");
+
 const myLibrary = ["Book 1", "Book 2", "Necronomicon"];
 
-function Book() {
-
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    // this.read = read;
 }
 
 function addBookToLibrary() {
+    const title = document.getElementById("title");
+    const author = document.getElementById("author");
+    const pages = document.getElementById("pages");
 
+    Book(title.textContent, author.textContent, pages.value);
 }
 
 function displayLibrary() {
-    for(let i = 0; i < myLibrary.length; i++) {
+    const library = document.getElementById("library");
 
+    for (let i = 0; i < myLibrary.length; i++) {
+        const bookCard = document.createElement("div");
+        const bookTitle = document.createElement("h3");
+        const deleteBook = document.createElement("button");
+
+        bookTitle.textContent = myLibrary[i];
+        bookCard.appendChild(bookTitle);
+        bookCard.setAttribute("class", "bookCard");
+        deleteBook.textContent = "Remove Book";
+        bookCard.appendChild(deleteBook);
+        library.appendChild(bookCard);
     }
 }
+
+addBookButton.addEventListener("mousedown", displayLibrary)
+
