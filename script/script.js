63fa186adcbcf8f3c2304c5e5e4c6acbd94efeb4
Karsten Leung <karstenkleung@gmail.com>
2023-09-13 00:12:31 -0400

    Add some display book function and book constructor

---
+++ script/script.js
@@ -0,0 +1,15 @@
+const myLibrary = ["Book 1", "Book 2", "Necronomicon"];
+
+function Book() {
+
+}
+
+function addBookToLibrary() {
+
+}
+
+function displayLibrary() {
+    for(let i = 0; i < myLibrary.length; i++) {
+
+    }
+}
