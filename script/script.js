4fa8b0ecc1347a31549a4b369ab73e392f688c4f
Karsten Leung <karstenkleung@gmail.com>
2023-09-19 23:22:07 -0400

    Add delete button and functionality

--- script/script.js
+++ script/script.js
@@ -1,38 +1,119 @@
 const addBookButton = document.getElementById("addBook");
+const library = document.getElementById("library");
 
-const myLibrary = ["Book 1", "Book 2", "Necronomicon"];
+const myLibrary = [];
 
-function Book(title, author, pages) {
+let bookIndex = 0;
+
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    // this.read = read;
+    this.read = read;
 }
 
 function addBookToLibrary() {
     const title = document.getElementById("title");
     const author = document.getElementById("author");
     const pages = document.getElementById("pages");
+    const readCheck = document.querySelectorAll('input[name="read-check"]');
 
-    Book(title.textContent, author.textContent, pages.value);
-}
+    if (checkForExistingBook(title.value, author.value, pages.value)) {
+        console.log("Book exists");
+        return true;
+    } else {
+        let read;
+        for (const radio of readCheck) {
+            if (radio.checked) {
+                read = radio.value;
+                break;
+            }
+        }
 
-function displayLibrary() {
-    const library = document.getElementById("library");
+        const newBook = new Book(title.value, author.value, pages.value, read);
 
-    for (let i = 0; i < myLibrary.length; i++) {
-        const bookCard = document.createElement("div");
-        const bookTitle = document.createElement("h3");
-        const deleteBook = document.createElement("button");
-
-        bookTitle.textContent = myLibrary[i];
-        bookCard.appendChild(bookTitle);
-        bookCard.setAttribute("class", "bookCard");
-        deleteBook.textContent = "Remove Book";
-        bookCard.appendChild(deleteBook);
-        library.appendChild(bookCard);
+        myLibrary.push(newBook);
+    }
+    // console.log(myLibrary);
+    title.value = "";
+    author.value = "";
+    pages.value = "";
+    for (const radio of readCheck) {
+        radio.checked = false;
     }
 }
 
-addBookButton.addEventListener("mousedown", displayLibrary)
+function displayLibrary(event) {
+    if (addBookToLibrary()) {
+        return;
+    } else {
+        for (bookIndex; bookIndex < myLibrary.length; bookIndex++) {
+            // Selectors
+            const bookCard = document.createElement("div");
+            const bookTitle = document.createElement("h3");
+            const bookAuthor = document.createElement("p");
+            const bookPages = document.createElement("p");
+            const deleteBook = document.createElement("button");
+            const readBook = document.createElement("p");
+            const readBookButton = document.createElement("button");
 
+            // Appending the content
+            bookTitle.textContent = myLibrary[bookIndex].title;
+            bookCard.appendChild(bookTitle);
+            bookAuthor.textContent = myLibrary[bookIndex].author;
+            bookCard.appendChild(bookAuthor);
+            bookPages.textContent = myLibrary[bookIndex].pages;
+            bookCard.appendChild(bookPages);
+            readBook.textContent = myLibrary[bookIndex].read;
+            bookCard.appendChild(readBook);
+
+            // Set class to book card
+            bookCard.setAttribute("class", "bookCard");
+
+            // Setting up delete book button
+            deleteBook.textContent = "Remove Book";
+            deleteBook.setAttribute("class", "deleteBook");
+
+            // Setting up read check button
+            readBookButton.textContent = "Change Read Status";
+            readBookButton.setAttribute('class', 'changeStatus');
+
+            // Appending delete and read status button
+            bookCard.appendChild(deleteBook).addEventListener('click', removeBook);
+            bookCard.appendChild(readBookButton).addEventListener('click', changeReadStatus);
+
+            // Appending book card to the library container
+            library.appendChild(bookCard);
+        }
+    }
+}
+
+function removeBook() {
+    // Select nodelist of the book cards
+    const bookCards = document.querySelectorAll(".bookCard");
+
+    // Get an Array version of the nodelist
+    const arrayOfBookCards = Array.from(bookCards);
+
+    let cardIndex = arrayOfBookCards.indexOf(this.closest('div'));
+
+    myLibrary.splice(cardIndex, 1);
+
+    this.closest('div').remove();
+
+    bookIndex--;
+}
+
+function checkForExistingBook(title, author, pages) {
+    for (let i = 0; i < myLibrary.length; i++) {
+        if (title == myLibrary[i].title && author == myLibrary[i].author && pages == myLibrary[i].pages) {
+            return true;
+        }
+    }
+}
+
+// function changeReadStatus() {
+//     console.log(this.closest("div").);
+// }
+
+addBookButton.addEventListener("mousedown", displayLibrary);
